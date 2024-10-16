a4f08d98625ac0473f69118c893eadb2e5b186e9
Irene Nagli <inagli@my.bcit.ca>
2024-10-15 21:49:36 -0700

    updated everything

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,15 @@
 //---------------------------------
-// Your own functions here
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
 //---------------------------------
 
 
